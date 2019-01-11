a32246e4a50b585060ada5f80c1cac7f7f7228e4
HibaGhadhab <hibaghadhab@ensg.eu>
2019-01-11 15:55:44 +0100

    handling existing operator in the begining

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,22 @@ import java.util.List;
 public class Main
 {
     //public static String CODE = "7 + 3 * 2 * ( 2 + 3 )";
-    public static String CODE = "( 2 + 5 ) * 3";
+    public static String CODE = "2+5*3";
 
     public static int position = 0;
-    public static List<Token> tokens; //à enlever
 
     public static void main (String args[]) {
         System.out.println("\n ************* Lexer *************");
         Lexer lexer = new Lexer(CODE);
-        //Lexer lexer = new Lexer(CODE, position, tokens);
         lexer.parse();
         lexer.getTokens().forEach(System.out::println);
 
-        System.out.println("\n ************* Parser *************");
-        System.out.println(Parser.parse(lexer.getTokens()));
+        System.out.println("\n ************* Parser Result *************");
+        Parser parser = new Parser();
+        System.out.println(parser.parse(lexer.getTokens()));
+        System.out.println(parser.sb);
+
+
 
 //        System.out.println("\n\nOther tests");
 //        "10 + 2 * 6";
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,7 +3,9 @@ import java.util.Stack;
 
 public class Parser
 {
-    public static int parse(List<Token> tokensFromLexer)
+    public StringBuilder sb = new StringBuilder();
+
+    public int parse(List<Token> tokensFromLexer)
     {
         // On récupère les tokens du lexeurs
         // On les mets dans un tableau de caractères
@@ -11,75 +13,89 @@ public class Parser
         for (Token token: tokensFromLexer)
         {
             String symbole = token.getSymbole();
-            chaine = chaine + " " +symbole;
+            chaine = chaine +" "+ symbole;
         }
         char[] tokens = chaine.toCharArray();
 
-        // pile des nombres: 'values'
-        Stack<Integer> values = new Stack<Integer>();
+        // pile des nombres: 'numberPile'
+        Stack<Integer> numberPile = new Stack<Integer>();
 
-        // pile des operateurs: 'ops'
-        Stack<Character> ops = new Stack<Character>();
+        // pile des operateurs: 'operatorPile'
+        Stack<Character> operatorPile = new Stack<Character>();
 
-        for (int i = 0; i < tokens.length; i++) //Parcours du tableau de caractères
+        //si premier caractère est un opérateur
+        if (tokens[1] == '+' || tokens[1] == '-' ||
+                tokens[1] == '*' || tokens[1] == '/')
         {
-            // le token courant espace à ignorer
-            if (tokens[i] == ' ')
-                continue;
-
-            //le token courant s'agit d'un nombre, ajouter le dans la pile des nombres
-            if (tokens[i] >= '0' && tokens[i] <= '9')
-            {
-                StringBuffer sbuf = new StringBuffer();
-                // si le nombre contient plus qu'un chiffre
-                // tant que i est un nombre, il s'arrête lorsqu'il trouve un operateur qu'il ne peut pas comparer avec 0 ou 9
-                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
-                    sbuf.append(tokens[i++]);
-                values.push(Integer.parseInt(sbuf.toString())); //ajout dans la pile des nombres
-            }
-
-            // token courant est une parenthèse ouvrante, ajouter à la pile des opérateurs
-            else if (tokens[i] == '(')
-                ops.push(tokens[i]);
-
-            // token courant est une parenthèse fermante
-            // si pas de parenthèse ouvrante, calculer avec deux pop de Values et l'opérateur pop ops
-            // pusher le résultat dans Values
-            else if (tokens[i] == ')')
-            {
-                while (ops.peek() != '(')
-                    values.push(calculer(ops.pop(), values.pop(), values.pop()));
-                ops.pop();
-            }
-
-            // token courant est un opérateur
-            else if (tokens[i] == '+' || tokens[i] == '-' ||
-                    tokens[i] == '*' || tokens[i] == '/')
-            {
-                // tantque la tete de la pile est plus prioritaire (ou meme priorité)
-                // que le token courant qui est un opérateur
-                // calculer avec deux pop de la pile Values et l'opérateur pop ops
-                // pusher le résultat dans la pile Values
-                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
-                    values.push(calculer(ops.pop(), values.pop(), values.pop()));
-
-                // Ajouter le token courant qui est un opérateur à la pile des opérateurs
-                ops.push(tokens[i]);
-            }
+            throw new
+                    UnsupportedOperationException("Une opération ne commence pas par un opérateur !");
         }
+        else
+        {
+
+            for (int i = 0; i < tokens.length; i++) //Parcours du tableau de caractères
+            {
+                // le token courant espace à ignorer
+                if (tokens[i] == ' ')
+                    continue;
+
+                //le token courant s'agit d'un nombre, ajouter le dans la pile des nombres
+                if (tokens[i] >= '0' && tokens[i] <= '9')
+                {
+                    StringBuffer sbuf = new StringBuffer();
+                    // si le nombre contient plus qu'un chiffre
+                    // tant que i est un nombre, il s'arrête lorsqu'il trouve un operateur qu'il ne peut pas comparer avec 0 ou 9
+                    while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
+                        sbuf.append(tokens[i++]);
+                    numberPile.push(Integer.parseInt(sbuf.toString())); //ajout dans la pile des nombres
+                }
+
+                // token courant est une parenthèse ouvrante, ajouter à la pile des opérateurs
+                else if (tokens[i] == '(')
+                    operatorPile.push(tokens[i]);
+
+                    // token courant est une parenthèse fermante
+                    // si pas de parenthèse ouvrante, calculer avec deux pop de numberPile et l'opérateur pop operatorPile
+                    // pusher le résultat dans numberPile
+                else if (tokens[i] == ')')
+                {
+                    while (operatorPile.peek() != '(')
+                        numberPile.push(calculer(operatorPile.pop(), numberPile.pop(), numberPile.pop()));
+
+                    operatorPile.pop();
+                }
+
+                // token courant est un opérateur
+                else if (tokens[i] == '+' || tokens[i] == '-' ||
+                        tokens[i] == '*' || tokens[i] == '/')
+                {
+                    // tantque la tete de la pile est plus prioritaire (ou meme priorité)
+                    // que le token courant qui est un opérateur
+                    // calculer avec deux pop de la pile numberPile et l'opérateur pop operatorPile
+                    // pusher le résultat dans la pile numberPile
+                    while (!operatorPile.empty() && plusPrioritaire(tokens[i], operatorPile.peek()))
+                        numberPile.push(calculer(operatorPile.pop(), numberPile.pop(), numberPile.pop()));
+
+                    // Ajouter le token courant qui est un opérateur à la pile des opérateurs
+                    operatorPile.push(tokens[i]);
+                }
+            }
+
+        }
+
 
         // tout le tableau token a été parcouru
         // faire les opérations restantes en popant les valeurs et l'opérateur et en pushant le résultat du calcul
-        while (!ops.empty())
-            values.push(calculer(ops.pop(), values.pop(), values.pop()));
+        while (!operatorPile.empty())
+            numberPile.push(calculer(operatorPile.pop(), numberPile.pop(), numberPile.pop()));
 
-        // Retourner la tete de la pile Values qui contient le résultat final
-        return values.pop();
+        // Retourner la tete de la pile numberPile qui contient le résultat final
+        return numberPile.pop();
     }
 
     // méthode pour trouver la priorité
     //retourne true si op2 est plus prioritaire (ou meme priorité) que op1
-    public static boolean hasPrecedence(char op1, char op2)
+    public static boolean plusPrioritaire(char op1, char op2)
     {
         if (op2 == '(' || op2 == ')')
             return false;
@@ -89,9 +105,15 @@ public class Parser
             return true;
     }
 
+
+
     //méthode calculer; selon l'opérateur
-    public static int calculer(char op, int b, int a)
+    public  int calculer(char op, int b, int a)
     {
+        sb.append(a);
+        sb.append(b);
+        sb.append(op);
+
         switch (op)
         {
             case '+':
@@ -103,11 +125,13 @@ public class Parser
             case '/':
                 if (b == 0)
                     throw new
-                            UnsupportedOperationException("Division par zéro impossible");
+                            UnsupportedOperationException("Division par zéro");
                 return a / b;
         }
+
         return 0;
     }
+
 
 
 }
